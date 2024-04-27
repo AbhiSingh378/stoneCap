9716623dcbcbe09bcc7f4170c196e493777f22bb
Abhishek Singh <singh.abhishek3@northeastern.edu>
2024-04-26 21:09:42 -0400

    version 4.0

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from lazypredict.Supervised import LazyRegressor
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
@@ -10,17 +10,25 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import base64
 import io
+
 #---------------------------------#
 # Page layout
 ## Page expands to full width
 st.set_page_config(page_title='The Algorithm Atlas Comparison App',
     layout='wide')
+
 #---------------------------------#
 # Model building
 def build_model(df):
     df = df.loc[:100] # FOR TESTING PURPOSE, COMMENT THIS OUT FOR PRODUCTION
     X = df.iloc[:,:-1] # Using all column except for the last column as X
     Y = df.iloc[:,-1] # Selecting the last column as Y
+
+    # Encode categorical features
+    categorical_features = []  # Replace with your categorical feature names
+    for feature in categorical_features:
+        le = LabelEncoder()
+        X[feature] = le.fit_transform(X[feature])
 
     st.markdown('**1.2. Dataset dimension**')
     st.write('X')
@@ -35,70 +43,69 @@ def build_model(df):
     st.info(Y.name)
 
     # Build lazy model
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y,test_size = split_size,random_state = seed_number)
-    reg = LazyRegressor(verbose=0,ignore_warnings=False, custom_metric=None)
-    models_train,predictions_train = reg.fit(X_train, X_train, Y_train, Y_train)
-    models_test,predictions_test = reg.fit(X_train, X_test, Y_train, Y_test)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=split_size, random_state=seed_number)
+    reg = LazyRegressor(verbose=0, ignore_warnings=False, custom_metric=None)
+    models_train, predictions_train = reg.fit(X_train, X_train, Y_train, Y_train)
+    models_test, predictions_test = reg.fit(X_train, X_test, Y_train, Y_test)
 
     st.subheader('2. Table of Model Performance')
 
     st.write('Training set')
     st.write(predictions_train)
-    st.markdown(filedownload(predictions_train,'training.csv'), unsafe_allow_html=True)
+    st.markdown(filedownload(predictions_train, 'training.csv'), unsafe_allow_html=True)
 
     st.write('Test set')
     st.write(predictions_test)
-    st.markdown(filedownload(predictions_test,'test.csv'), unsafe_allow_html=True)
+    st.markdown(filedownload(predictions_test, 'test.csv'), unsafe_allow_html=True)
 
     st.subheader('3. Plot of Model Performance (Test set)')
 
-
     with st.markdown('**R-squared**'):
         # Tall
-        predictions_test["R-Squared"] = [0 if i < 0 else i for i in predictions_test["R-Squared"] ]
+        predictions_test["R-Squared"] = [0 if i < 0 else i for i in predictions_test["R-Squared"]]
         plt.figure(figsize=(3, 9))
         sns.set_theme(style="whitegrid")
         ax1 = sns.barplot(y=predictions_test.index, x="R-Squared", data=predictions_test)
         ax1.set(xlim=(0, 1))
-    st.markdown(imagedownload(plt,'plot-r2-tall.pdf'), unsafe_allow_html=True)
+        st.markdown(imagedownload(plt, 'plot-r2-tall.pdf'), unsafe_allow_html=True)
         # Wide
-    plt.figure(figsize=(9, 3))
-    sns.set_theme(style="whitegrid")
-    ax1 = sns.barplot(x=predictions_test.index, y="R-Squared", data=predictions_test)
-    ax1.set(ylim=(0, 1))
-    plt.xticks(rotation=90)
-    st.pyplot(plt)
-    st.markdown(imagedownload(plt,'plot-r2-wide.pdf'), unsafe_allow_html=True)
+        plt.figure(figsize=(9, 3))
+        sns.set_theme(style="whitegrid")
+        ax1 = sns.barplot(x=predictions_test.index, y="R-Squared", data=predictions_test)
+        ax1.set(ylim=(0, 1))
+        plt.xticks(rotation=90)
+        st.pyplot(plt)
+        st.markdown(imagedownload(plt, 'plot-r2-wide.pdf'), unsafe_allow_html=True)
 
     with st.markdown('**RMSE (capped at 50)**'):
         # Tall
-        predictions_test["RMSE"] = [50 if i > 50 else i for i in predictions_test["RMSE"] ]
+        predictions_test["RMSE"] = [50 if i > 50 else i for i in predictions_test["RMSE"]]
         plt.figure(figsize=(3, 9))
         sns.set_theme(style="whitegrid")
         ax2 = sns.barplot(y=predictions_test.index, x="RMSE", data=predictions_test)
-    st.markdown(imagedownload(plt,'plot-rmse-tall.pdf'), unsafe_allow_html=True)
+        st.markdown(imagedownload(plt, 'plot-rmse-tall.pdf'), unsafe_allow_html=True)
         # Wide
-    plt.figure(figsize=(9, 3))
-    sns.set_theme(style="whitegrid")
-    ax2 = sns.barplot(x=predictions_test.index, y="RMSE", data=predictions_test)
-    plt.xticks(rotation=90)
-    st.pyplot(plt)
-    st.markdown(imagedownload(plt,'plot-rmse-wide.pdf'), unsafe_allow_html=True)
+        plt.figure(figsize=(9, 3))
+        sns.set_theme(style="whitegrid")
+        ax2 = sns.barplot(x=predictions_test.index, y="RMSE", data=predictions_test)
+        plt.xticks(rotation=90)
+        st.pyplot(plt)
+        st.markdown(imagedownload(plt, 'plot-rmse-wide.pdf'), unsafe_allow_html=True)
 
     with st.markdown('**Calculation time**'):
         # Tall
-        predictions_test["Time Taken"] = [0 if i < 0 else i for i in predictions_test["Time Taken"] ]
+        predictions_test["Time Taken"] = [0 if i < 0 else i for i in predictions_test["Time Taken"]]
         plt.figure(figsize=(3, 9))
         sns.set_theme(style="whitegrid")
         ax3 = sns.barplot(y=predictions_test.index, x="Time Taken", data=predictions_test)
-    st.markdown(imagedownload(plt,'plot-calculation-time-tall.pdf'), unsafe_allow_html=True)
+        st.markdown(imagedownload(plt, 'plot-calculation-time-tall.pdf'), unsafe_allow_html=True)
         # Wide
-    plt.figure(figsize=(9, 3))
-    sns.set_theme(style="whitegrid")
-    ax3 = sns.barplot(x=predictions_test.index, y="Time Taken", data=predictions_test)
-    plt.xticks(rotation=90)
-    st.pyplot(plt)
-    st.markdown(imagedownload(plt,'plot-calculation-time-wide.pdf'), unsafe_allow_html=True)
+        plt.figure(figsize=(9, 3))
+        sns.set_theme(style="whitegrid")
+        ax3 = sns.barplot(x=predictions_test.index, y="Time Taken", data=predictions_test)
+        plt.xticks(rotation=90)
+        st.pyplot(plt)
+        st.markdown(imagedownload(plt, 'plot-calculation-time-wide.pdf'), unsafe_allow_html=True)
 
 # Download CSV data
 # https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
@@ -139,7 +146,6 @@ with st.sidebar.header('2. Set Parameters'):
     split_size = st.sidebar.slider('Data split ratio (% for Training Set)', 10, 90, 80, 5)
     seed_number = st.sidebar.slider('Set the random seed number', 1, 100, 42, 1)
 
-
 #---------------------------------#
 # Main panel
 
@@ -158,7 +164,7 @@ else:
         diabetes = load_diabetes()
         X = pd.DataFrame(diabetes.data, columns=diabetes.feature_names)
         Y = pd.Series(diabetes.target, name='response')
-        df = pd.concat( [X,Y], axis=1 )
+        df = pd.concat([X, Y], axis=1)
 
         st.markdown('The Diabetes dataset is used as the example.')
         st.write(df.head(5))
@@ -167,7 +173,7 @@ else:
         california = fetch_california_housing()
         X = pd.DataFrame(california.data, columns=california.feature_names).loc[:100] # FOR TESTING PURPOSE, COMMENT THIS OUT FOR PRODUCTION
         Y = pd.Series(california.target, name='MedHouseVal').loc[:100] # FOR TESTING PURPOSE, COMMENT THIS OUT FOR PRODUCTION
-        df = pd.concat( [X,Y], axis=1 )
+        df = pd.concat([X, Y], axis=1)
 
         st.markdown('The California housing dataset is used as the example.')
         st.write(df.head(5))
